19d81f9595beadd842237285a3dd8f4c0149d7f6
lowresboots <lowresboots@gmail.com>
2024-11-04 05:19:57 -0500

    Main content, theme change, additional fixes

--- script.js
+++ script.js
@@ -17,3 +17,14 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+const theme = localStorage.getItem('theme') || 'light';
+document.documentElement.setAttribute('data-theme', theme);
+
+function toggleTheme() {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}
